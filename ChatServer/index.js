bf9150b7cf91f16e9ded40d0fd6a8e5a44fcb4b6
Shinono <85401098+K93MSY@users.noreply.github.com>
2022-06-13 16:33:54 +0900

    Update index.js

--- ChatServer/index.js
+++ ChatServer/index.js
@@ -1,1 +1,14 @@
-//まだなんも書いてません（実力不足;;
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
